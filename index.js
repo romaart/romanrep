3d6233f3470f6df26cf2c60bf428fe518fcae602
romaart <49924137+romaart@users.noreply.github.com>
2025-06-10 23:40:53 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,56 @@
+const fs = require("fs");
+const express = require("express");
+const TelegramBot = require("node-telegram-bot-api");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const botToken = "7595457842:AAG10ASgQj-2C01EUVJ9cXcWDYiodQ02fI4";
+
+const app = express();
+const bot = new TelegramBot(botToken, { polling: true });
+
+app.use(bodyParser.json());
+app.use(cors());
+
+let dataBase = require("./chats.json");
+
+bot.on("message", (msg) => {
+  const chatId = msg.chat.id;
+  console.log(chatId);
+
+  if (!dataBase.chats.includes(chatId)) {
+    dataBase.chats.push(chatId);
+    fs.writeFileSync("chats.json", JSON.stringify(dataBase));
+  }
+});
+
+const sendToAllChats = (message) => {
+  dataBase.chats.forEach((chatId) => {
+    bot.sendMessage(chatId, message);
+  });
+};
+
+app.get("/", (req, res) => {
+  console.log("alive");
+  res.json({ message: "alive" });
+});
+
+app.post("/", (req, res) => {
+  const keys = Object.keys(req.body);
+  let data = "";
+
+  keys.forEach((key) => {
+    data += `${key}: ${req.body[key]}` + "\n";
+  });
+
+  sendToAllChats(data);
+
+  res.json({
+    message: "success",
+  });
+});
+
+app.listen(process.env.PORT || 5000, (err) => {
+  if (err) return console.log(err);
+  console.log("Start");
+});
